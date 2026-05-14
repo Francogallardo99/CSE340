458727fbfb81325986b00962d657b0e499fd89b5
Franco Gallardo <frankogallardo444@gmail.com>
2026-05-14 11:27:44 -0300

    listo

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ import path from 'path';
 
 import { testConnection } from './src/models/db.js';
 
+import { getAllOrganizations } from './src/models/organizations.js';
+
+
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 
@@ -39,8 +42,10 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
     const title = 'Our Partner Organizations';
-    res.render('organizations', { title });
+
+    res.render('organizations', { title, organizations });
 });
 
 app.get('/projects', async (req, res) => {
