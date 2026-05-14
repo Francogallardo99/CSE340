171e26ed6848ac88652783ffe2562195b0c6e598
Franco Gallardo <frankogallardo444@gmail.com>
2026-05-14 12:11:21 -0300

    A

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.get('/', async (req, res) => {
 
 app.get('/organizations', async (req, res) => {
     const organizations = await getAllOrganizations();
-    const title = 'Organizations';
+    const title = 'Our Partner Organizations';
 
     res.render('organizations', { title, organizations });
 });
