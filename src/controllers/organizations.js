43b7ab88365e978727cbfd98a299451beb45f2aa
Franco Gallardo <frankogallardo444@gmail.com>
2026-05-25 21:17:45 -0300

    listo

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -2,6 +2,7 @@
 import { getAllOrganizations } from '../models/organizations.js';
 import { getOrganizationDetails } from '../models/organizations.js';
 import { getProjectsByOrganizationId } from '../models/projects.js';
+import { createOrganization } from '../models/organizations.js';
 
 // Define any controller functions
 const showOrganizationsPage = async (req, res) => {
@@ -22,5 +23,20 @@ const showOrganizationDetailsPage = async (req, res) => {
     
     res.render('organization', { title, organization, projects });
 };
+
+const showNewOrganizationForm = async (req, res) => {
+    const title = 'Add New Organization';
+
+    res.render('new-organization', { title });
+}
+
+const processNewOrganizationForm = async (req, res) => {
+    const { name, description, contactEmail } = req.body;
+    const logoFilename = 'placeholder-logo.png'; // Use the placeholder logo for all new organizations
+
+    const organizationId = await createOrganization(name, description, contactEmail, logoFilename);
+    res.redirect(`/organization/${organizationId}`);
+};
+
 // Export any controller functions
-export { showOrganizationsPage, showOrganizationDetailsPage };
+export { showOrganizationsPage, showOrganizationDetailsPage, showNewOrganizationForm, processNewOrganizationForm };
